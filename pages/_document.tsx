32416d09354efb4533d4a1e1e3e08bfe04196505
HiroMinase <lostsymbol612@gmail.com>
2022-09-23 17:15:18 +0900

    eslintエラーの対応

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import React from 'react';
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@500&display=swap"
           rel="stylesheet"
@@ -17,4 +18,4 @@ export default function Document() {
       </body>
     </Html>
   );
-};
+}
