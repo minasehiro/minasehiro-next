b0c2a50825453a092a5881b1b4982807fa2887c3
HiroMinase <lostsymbol612@gmail.com>
2022-09-23 14:37:23 +0900

    HEROイメージのスタイル調整

--- pages/app.tsx
+++ pages/app.tsx
@@ -23,41 +23,6 @@ const App: NextPage = () => {
           <div className={styles.homeImage}>
             <Image src="/assets/images/photograph.jpg" layout="fill" objectFit="contain" priority={true} />
           </div>
-
-          <div className={styles.apps}>
-            <Link href="/photographs/nature" scroll={false}>
-              <a>
-                <div className={styles.appIcon}>
-                  <Image src="/assets/images/nature_icon.jpg" layout="fill" objectFit="contain" />
-                </div>
-                Nature
-              </a>
-            </Link>
-            <Link href="/photographs/portrait" scroll={false}>
-              <a>
-                <div className={styles.appIcon}>
-                  <Image src="/assets/images/portrait_icon.jpg" layout="fill" objectFit="contain" />
-                </div>
-                Portrait
-              </a>
-            </Link>
-            <Link href="/photographs/snap" scroll={false}>
-              <a>
-                <div className={styles.appIcon}>
-                  <Image src="/assets/images/snap_icon.jpg" layout="fill" objectFit="contain" />
-                </div>
-                Snap
-              </a>
-            </Link>
-            <Link href="/photographs/base_wishes" scroll={false}>
-              <a>
-                <div className={styles.appIcon}>
-                  <Image src="/assets/images/base_wishes_icon.jpg" layout="fill" objectFit="contain" />
-                </div>
-                Best wishes
-              </a>
-            </Link>
-          </div>
         </main>
 
         <Footer />
